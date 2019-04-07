4e123aa907fdd83c345947ad1722d36cecf10d80
Christopher Childers <christopher.childers13@yahoo.com>
2019-04-06 21:22:35 -0400

    sets up the server to run the fortunes api

---
+++ index.js
@@ -0,0 +1,10 @@
+const express = require('express');
+const port = 3000;
+
+const app = express();
+
+app.get('/fortunes', (req, res) => {
+    res.send('requesting fortunes');
+});
+
+app.listen(port, () => console.log(`listening on port number ${port}`));
